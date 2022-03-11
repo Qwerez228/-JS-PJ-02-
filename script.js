e257ca1f005454e0527c40d8533488f53f50ec63
Qwerez228 <85694012+Qwerez228@users.noreply.github.com>
2022-03-11 12:36:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+/* Индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция сладера */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("item");
+    let Rostov = document.getElementsByClassName("Sochi");
+    let dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    for (i = 0; i < Rostov.length; i++) {
+        Rostov[i].className = Rostov[i].className.replace(" active1", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    Rostov[slideIndex - 1].className += " active1";
+}
